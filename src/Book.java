fec4f40f42e21b5e8cbe3a4442cb9356d2f9f774
bazhina-angry-bunny <praga617@mail.ru>
2024-02-25 23:50:03 +0500

    Tasks for working with objects and classes

---
+++ src/Book.java
@@ -0,0 +1,56 @@
+import java.util.Objects;
+
+public class Book {
+    private String bookTitle;
+    private Author authorTheFullName;
+    private int yearOfPublication;
+
+// метод-конструктор
+    public Book(String bookTitle, Author authorTheFullName, int yearOfPublication) {
+        this.bookTitle = bookTitle;
+        this.authorTheFullName = authorTheFullName;
+        this.yearOfPublication = yearOfPublication;
+    }
+    // сеттеры и геттеры
+    public String getBookTitle() {
+        return bookTitle;
+    }
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public Author getAuthorTheFullName() {
+        return authorTheFullName;
+    }
+    public void setAuthorTheFullName(Author authorTheFullName) {
+        this.authorTheFullName = authorTheFullName;
+    }
+//// Метод toString() преобразовывает данные сложного объекта в одну строку
+    @Override
+    public String toString() {
+        return  "Книга под названием " + bookTitle +
+                ", автор " + authorTheFullName +
+                ", издана в " + yearOfPublication + " году.";
+    }
+    // методы equals и hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(bookTitle, book.bookTitle) && Objects.equals(authorTheFullName, book.authorTheFullName);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, authorTheFullName, yearOfPublication);
+    }
+
+
+}
